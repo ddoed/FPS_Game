fdd62c9f0d0b3ef3175533f36bb125716e3c01a9
ddoed <yimnabin20051022@gmail.com>
2025-01-03 20:53:37 +0900

    Add Heal Block Sound

--- Assets/Scripts/Interact/Item/Cube.cs
+++ Assets/Scripts/Interact/Item/Cube.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Cube : MonoBehaviour, IInteract
 {
     public GameObject player;
+    public AudioSource healSound;
     public void Interact()
     {
         PlayerHealthUI healthUI = player.GetComponent<PlayerHealthUI>();
         if(healthUI.score >= 30)
         {
+            healSound.Play();
             healthUI.RestoreHealth(10);
             healthUI.DecreaseCoin(30);
         }
